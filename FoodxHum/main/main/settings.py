da26dcf6366cf482ab8f3708f7746a53d58a7986
ambag12 <ambaigbaig@gmail.com>
2025-04-01 21:02:06 +0500

    files

--- FoodxHum/main/main/settings.py
+++ FoodxHum/main/main/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "public"
+STATIC_ROOT = os.path.join(BASE_DIR, "FoodxHum", "main", "public")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
